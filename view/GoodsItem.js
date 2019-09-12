1deeafb4c2cd7041466c7d98253ac20e4c515336
julie <771635995@qq.com>
2019-09-12 14:17:15 +0800

    julie

---
+++ view/GoodsItem.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+
+
+type Props = {};
+export default class GoodsItem extends Component<Props> {
+    constructor(props) {
+        super(props);
+        console.info(props)
+        this.state = {
+            size: 1,
+            check: false,
+        }
+
+    }
+    itemAdd() {
+        this.setState({ size: this.state.size + 1 })
+    }
+    itemMin() {
+        if (this.state.size == 1) { return }
+        this.setState({ size: this.state.size - 1 })
+    }
+    render() {
+
+        const navigate = this.props.navigation;
+        //此处可以自定义跳转属性
+        return (
+            <View >
+                <View style={{ height: 112 }}>
+                    <View style={{ width: '90%', alignSelf: 'center', alignItems: 'center', flex: 1, flexDirection: 'row', flexWrap: 'nowrap' }}>
+                        <View style={{ width: '7%',marginRight:5}}>
+                            <TouchableOpacity onPress={() => { this.state.check == true ? this.setState({ check: false }) : this.setState({ check: true }) }}>
+                                <Image style={{ width: 23, height: 23 }} source={this.state.check == false ? require('../image/icons/tick.png') : require('../image/icons/tick-ok.png')} />
+                            </TouchableOpacity>
+
+                        </View>
+                        <View style={{ width: '26%' }}>
+                            <Image style={{ width: 62, height: 67, alignSelf: 'center' }} source={this.props.imgpath} resizeMode='center' />
+                        </View>
+                        <View style={{ width: '43%' }}>
+                            <Text style={{ fontFamily: 'NotoSansHans-Light', height: 34, width: 200, color: '#0071bc' }}>{this.props.title}</Text>
+                            <Text style={{ fontFamily: 'NotoSansHans-Light', height: 23, width: 178, fontSize: 12, lineHeight: 26 }}>{this.props.intro}</Text>
+                            <View style={{ width: 78, height: 23, flexDirection: 'row' }}>
+                                <Text style={{ height: 23, fontFamily: 'NotoSansHans-Light', lineHeight: 31, textAlign: 'center', textDecorationLine: 'line-through' }}>{this.props.oldprice}</Text>
+                                <Text style={{ height: 23, fontFamily: 'NotoSansHans-Light', lineHeight: 28, textAlign: 'center', justifyContent: 'center', color: '#f15a24', fontSize: 18 }}>&nbsp;&nbsp;{this.props.newprice}</Text>
+                                <Text style={{ height: 23, fontFamily: 'NotoSansHans-Light', lineHeight: 28, textAlign: 'center', justifyContent: 'center', color: '#f15a24', fontSize: 18 }}>&nbsp;&nbsp;{this.props.unit}</Text>
+                            </View>
+                        </View>
+                        <View style={{ width: '29%', marginTop: 56 }}>
+                            <View style={{ height: 36, width: 78, alignItems: 'flex-start', justifyContent: 'space-between', flexDirection: 'row' }}>
+                                <TouchableOpacity onPress={this.itemMin.bind(this)}>
+                                    <Image style={{ width: 24, height: 24, alignSelf: 'center', marginTop: 5 }} source={require('../image/icons/min.png')} resizeMode='contain' />
+                                </TouchableOpacity>
+                                <Text style={{ alignSelf: 'center', fontFamily: 'NotoSansHans-Light', fontWeight: '700', textAlign: 'center' }}>{this.state.size}</Text>
+                                <TouchableOpacity onPress={this.itemAdd.bind(this)}>
+                                    <Image style={{ width: 24, height: 24, alignSelf: 'center', marginTop: 5 }} source={require('../image/icons/plus.png')} resizeMode='contain' />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
--- view/TabMallActivity.js
+++ view/TabMallActivity.js
@@ -2,19 +2,15 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, Button, Modal, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview'
 import { TextInput } from 'react-native-gesture-handler';
+import GoodsItem from './GoodsItem';
 
 export default class MallActivity extends Component<Props> {
 
     constructor(props) {
         super(props);
-        this.add = this.add.bind(this)
-        this.min = this.min.bind(this)
         this.state = {
             display: false,
-            size: 1,
-            check: false,
         }
-
 
         //定义成员变量
         this.mail = "1058633117@qq.com";
@@ -24,13 +20,6 @@ export default class MallActivity extends Component<Props> {
     }
 
 
-    add() {
-        this.setState({ size: this.state.size + 1 })
-    }
-    min() {
-        if (this.state.size == 1) { return }
-        this.setState({ size: this.state.size - 1 })
-    }
     render() {
         return (
             <View>
@@ -50,41 +39,28 @@ export default class MallActivity extends Component<Props> {
                  */}
                 <Text style={{ paddingLeft: 20, paddingTop: 12, fontFamily: 'NotoSansHans-Light', fontSize: 26, backgroundColor: '#ffffff' }}>Health Market</Text>
                 <Text style={{ paddingLeft: 20, color: '#e95a24', backgroundColor: '#efefef', fontFamily: 'NotoSansHans-Light' }}>Tip: An additional 20 USD courier fee is required</Text>
+                <View style={{ alignItems: 'center' }}>
+                    <GoodsItem
+                        id={"1"}
+                        imgpath={require('../image/icons/DNA.png')}
+                        title={"Biological age detection2.0"}
+                        intro={"Saliva DNA Collection Kit"} 
+                        oldprice={"$198"}
+                        newprice={"$99"}
+                        unit={"/kit"}
+                        />
 
-                <View style={{ height: 112 }}>
-                    <View style={{ width: '90%', alignSelf: 'center', alignItems: 'center', flexDirection: 'row', flexWrap: 'nowrap' }}>
-                        <View style={{ width: '7%' }}>
-                            <TouchableOpacity onPress={() => { this.state.check == true ? this.setState({ check: false }) : this.setState({ check: true }) }}>
-                                <Image style={{ width: '100%', height: 23 }} source={this.state.check == false ?
-                                    require('../image/icons/tick.png') : require('../image/icons/tick-ok.png')} />
-                            </TouchableOpacity>
-
-                        </View>
-                        <View style={{ width: '26%', }}>
-                            <Image style={{ width: 67, height: 67, alignSelf: 'center' }} source={require('../image/icons/DNA.png')} resizeMode='center' />
-                        </View>
-                        <View style={{ width: '43%' }}>
-                            <Text style={{ fontFamily: 'NotoSansHans-Light', height: 34, width: 200, color: '#0071bc' }}>Biological age detection2.0</Text>
-                            <Text style={{ fontFamily: 'NotoSansHans-Light', height: 23, width: 178, fontSize: 12, lineHeight: 26 }}>Saliva DNA Collection Kit</Text>
-                            <View style={{ width: 78, height: 23, flexDirection: 'row' }}>
-                                <Text style={{ height: 23, fontFamily: 'NotoSansHans-Light', lineHeight: 31, textAlign: 'center', textDecorationLine: 'line-through' }}>$198</Text>
-                                <Text style={{ height: 23, fontFamily: 'NotoSansHans-Light', lineHeight: 28, textAlign: 'center', justifyContent: 'center', color: '#f15a24', fontSize: 18 }}>  &nbsp;&nbsp;$99</Text>
-                            </View>
-                        </View>
-                        <View style={{ width: '29%', marginTop: 56 }}>
-                            <View style={{ height: 36, width: 78, alignItems: 'flex-start', justifyContent: 'space-between', flexDirection: 'row' }}>
-                                <TouchableOpacity onPress={this.min.bind(this)}>
-                                    <Image style={{ width: 24, height: 24, alignSelf: 'center', marginTop: 5 }} source={require('../image/icons/min.png')} resizeMode='contain' />
-                                </TouchableOpacity>
-                                <Text style={{ alignSelf: 'center', fontFamily: 'NotoSansHans-Light', fontWeight: '700', textAlign: 'center' }}>{this.state.size}</Text>
-                                <TouchableOpacity onPress={this.add.bind(this)}>
-                                    <Image style={{ width: 24, height: 24, alignSelf: 'center', marginTop: 5 }} source={require('../image/icons/plus.png')} resizeMode='contain' />
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </View>
+                       
+                    <View style={{ backgroundColor: '#efefef', height: 12, width: '100%' }}></View>
+                    <GoodsItem
+                        id={"2"}
+                        imgpath={require('../image/icons/SAM-e.png')}
+                        title={"SAM-e"}
+                        intro={"S-adenosine Supplement"}
+                        oldprice={"$88"}
+                        newprice={"$60"} 
+                        unit={"/box"}/>
                 </View>
-                <View style={{ backgroundColor: '#efefef', height: 23, width: '100%' }}></View>
             </View >
 
         );
